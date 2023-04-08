c8f7c020ac39ea1d0a026640afc4110b35c78222
TeemSucks <breadmanlol@proton.me>
2023-04-07 20:02:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+import plyer
+
+timeBetweenEachCheck = 120 # change this to any amount (2 minutes)
+
+url = 'https://www.roblox.com/catalog?Category=11&Subcategory=19&CurrencyType=3&pxMin=0&pxMax=0&salesTypeFilter=1&SortType=3&IncludeNotForSale'
+
+id_file = 'ids.txt'
+
+while True:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    items = soup.find_all('div', class_='item-card')
+    limiteds = []
+    for item in items:
+        if 'Limited' in item.text:
+            item_id = item['data-item-id']
+            item_name = item.find('a', class_='text-name').text.strip()
+            item_link = 'https://www.roblox.com/catalog/' + item_id + '/' + item_name.replace(' ', '-')
+
+            if item_id not in open(id_file).read():
+                limiteds.append((item_name, item_link))
+
+                #print("Name: ", item_name, "\n", item_link, "\n\n")
+
+                with open(id_file, 'a') as f:
+                    f.write(item_id + '\n')
+
+    if limiteds:
+        print('New limited!')
+        for limited in limiteds:
+            print(limited[0] + ': ' + limited[1])
+            plyer.notification.notify(title='New free limited!', message=limited[0] + ': ' + limited[1])
+
+    time.sleep(timeBetweenEachCheck)
